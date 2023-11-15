9c09303113ba6e19434c668005842b376d1e5c69
MarcoEsquivel1 <marco.esquivel@catolica.edu.sv>
2023-11-15 15:40:38 -0600

    feat(UI): :lipstick: Parallax and Containers
    Add parallax animation to homepage and improve the containers' dimensions

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,19 +5,21 @@ import { skills } from '../data/skills';
 export const Home = () => {
 
     return(
-        <div id='home' className="containerHome">
-            <div className="textHome">
-                <h1>Marco <span>René</span> Esquivel <span>Juárez</span></h1>
-                <p><span>Frontend</span> and <span>Backend</span> developer specialized in creating and consuming Restful APIs with knowledge in technologies such as:</p>
-                <p>{
-                    skills.map((skill, index) => {
-                        return <><span key={index}>{skill.name}</span>  / </>
-                    })    
-                }</p>
-                <ButtonsContainer />
-            </div>
-            <div className="imageHome">
-                <img src={pcImage} alt="pc"/>
+        <div id='home' className="containerHome parallax">
+            <div className="content">
+                <div className="textHome">
+                    <h1>Marco <span>René</span> Esquivel <span>Juárez</span></h1>
+                    <p><span>Frontend</span> and <span>Backend</span> developer specialized in creating and consuming Restful APIs with knowledge in technologies such as:</p>
+                    <p>{
+                        skills.map((skill, index) => {
+                            return <><span key={index}>{skill.name}</span>  / </>
+                        })    
+                    }</p>
+                    <ButtonsContainer />
+                </div>
+                <div className="imageHome">
+                    <img src={pcImage} alt="pc"/>
+                </div>
             </div>
         </div>
     )
