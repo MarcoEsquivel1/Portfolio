cd25b5f695e0b55cf96a0cc32762b74de5c1b18d
MarcoEsquivel1 <marco.esquivel@catolica.edu.sv>
2023-11-30 15:13:10 -0600

    feat(animations): :lipstick: Particles
    Add particles to the UI

---
+++ src/components/Common/ParticlesContainer.tsx
@@ -0,0 +1,87 @@
+import { Particles } from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import React, { useCallback } from "react";
+import type { Engine } from "tsparticles-engine";
+
+export const ParticlesContainer = () => {
+    const particlesInit = useCallback(async (engine: Engine) => {
+        await loadFull(engine);
+    }, [])
+
+    const particlesLoaded = useCallback(async () => {}, [])
+
+    return <Particles 
+    id="tsparticles" 
+    init={particlesInit} 
+    loaded={particlesLoaded} 
+    style={{ position: "absolute", width: "100%", height: "100%", zIndex: 1 }}
+    options={{
+        fullScreen: { enable: false },
+        background: {
+            color: {
+                value: "",
+            },
+        },
+        fpsLimit: 120,
+        interactivity: {
+            events: {
+                onClick: {
+                    enable: false,
+                    mode: "push",
+                },
+                onHover: {
+                    enable: true,
+                    mode: "repulse",
+                },
+                resize: true,
+            },
+            modes: {
+                push: {
+                    quantity: 90,
+                },
+                repulse: {
+                    distance: 200,
+                    duration: 0.4,
+                },
+            },
+        },
+        particles:{
+            color: {
+                value: '#fff'
+            },
+            links: {
+                enable: false,
+            },
+            collisions:{
+                enable: true, 
+            },
+            move:{
+                direction: 'none',
+                enable: true,
+                outModes:{
+                    default: 'bounce',
+                },
+                random: false,
+                speed: 1,
+                straight: false,
+            },
+            number: {
+                density: {
+                    enable: true,
+                    value_area: 800,
+                },
+                value: 20,
+            },
+            opacity: {
+                value: 0.5,
+            },
+            shape:{
+                type: 'circle',
+            },
+            size: {
+                value: { min: 1, max: 5 },
+            },
+        },
+        detectRetina: true,
+    }} />
+} 
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,8 +1,13 @@
+
 import animatedTransition from '../animation/animatedTransition';
+import { ParticlesContainer } from '../components/Common/ParticlesContainer';
 export const About = animatedTransition(() => {
     return(
-        <div id="about" className="container">
-            <h1>About</h1>
-        </div>
+        <>
+            <ParticlesContainer />
+            <div id="about" className="container">
+                <h1>About</h1>
+            </div>
+        </>
     );
 })
